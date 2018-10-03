f3c0dad3e68a421b050bde0a57a924e734b4d6f9
CORP\b_jackson <b_jackson@wargaming.net>
2018-10-03 12:17:00 -0700

    Another small change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">Hello World</div>
+      <div className="App">Lorem Ipsum</div>
     );
   }
 }
